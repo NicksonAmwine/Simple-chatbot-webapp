0d0370dfc0b88781119132ae930019dae4c2514e
Nickson Amwine <amwinenickson7.com>
2024-01-09 20:40:41 +0300

    moved chatbot.css and chatbot.js to staticfiles/mychatApp/templates/css and js

--- mychatbot/settings.py
+++ mychatbot/settings.py
@@ -153,7 +153,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR, 'static'
+    os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
